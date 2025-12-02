89f0e38b781d7381f378e0b98a417c7bab3f0b0e
Amany <amanysuliman2002@gmail.com>
2025-12-02 03:11:21 +0200

    Add project files.

---
+++ app1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Number of small carpets : ");
+int numCarpetsSmall = Convert.ToInt32(Console.ReadLine());
+Console.Write("Number of large carpets : ");
+int numCarpetsLarge = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Price per small room: $25");
+Console.WriteLine("Price per large room: $35");
+decimal totalCost = (numCarpetsSmall * 25) + (numCarpetsLarge * 35);
+decimal tax = totalCost * 0.06m;
+Console.WriteLine($" Cost : {totalCost:C}");
+Console.WriteLine($" Tax : {tax:C}");
+Console.WriteLine(" =========================================================");
+
+Console.WriteLine($" Total estimate: {totalCost + tax:C}");
+Console.WriteLine("This estimate is valid for 30 days");
+
+
+
+
+
